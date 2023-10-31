4d2b82f2cfcc41d9fb863688efa8e75b1814610b
Leo Lin <leosunday31@gmail.com>
2023-10-30 22:24:55 -0400

    animation input control complete

--- Assets/Scripts/inputManager.cs
+++ Assets/Scripts/inputManager.cs
@@ -10,10 +10,12 @@ public class TimeController : MonoBehaviour
     private InputAction skyboxAction;
     private InputAction volumeAction;
 
-    private Animator chickenAnimator;
     private InputAction chickenAnimActionA;
     private InputAction chickenAnimActionB;
     private InputAction chickenAnimActionC;
+    private InputAction chickenAnimActionD;
+    private InputAction chickenAnimActionE;
+    private InputAction chickenAnimActionF;
 
     private void Awake()
     {
@@ -37,15 +39,38 @@ public class TimeController : MonoBehaviour
         }
 
         InputActionMap sendAMap = actionAsset.FindActionMap("Send A");
-        chickenAnimator = GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>();
+        // GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>() = GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>();
 
         chickenAnimActionA = sendAMap.FindAction("A");
         chickenAnimActionB = sendAMap.FindAction("B");
         chickenAnimActionC = sendAMap.FindAction("C");
+        chickenAnimActionD = sendAMap.FindAction("D");
+        chickenAnimActionE = sendAMap.FindAction("E");
+        chickenAnimActionF = sendAMap.FindAction("F");
 
         if (chickenAnimActionA != null)
         {
             chickenAnimActionA.performed += chickenAnimAInputChanged;
+        }
+        if (chickenAnimActionB != null)
+        {
+            chickenAnimActionB.performed += chickenAnimBInputChanged;
+        }
+        if (chickenAnimActionC != null)
+        {
+            chickenAnimActionC.performed += chickenAnimCInputChanged;
+        }
+        if (chickenAnimActionD != null)
+        {
+            chickenAnimActionD.performed += chickenAnimDInputChanged;
+        }
+        if (chickenAnimActionE != null)
+        {
+            chickenAnimActionE.performed += chickenAnimEInputChanged;
+        }
+        if (chickenAnimActionF != null)
+        {
+            chickenAnimActionF.performed += chickenAnimFInputChanged;
         }
 
 #if !UNITY_EDITOR
@@ -93,7 +118,28 @@ public class TimeController : MonoBehaviour
     {
         float value = context.ReadValue<float>();
 
-        GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>().SetFloat("Spin", value);
+        GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>().SetFloat("Stepping", value);
+    }
+
+    private void chickenAnimDInputChanged(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+
+        GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>().SetFloat("Twerk", value);
+    }
+
+    private void chickenAnimEInputChanged(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+
+        GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>().SetFloat("Twist", value);
+    }
+
+    private void chickenAnimFInputChanged(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+
+        GetComponent<chickenManager>().mainChickenObject.transform.Find("Body").GetComponent<Animator>().SetFloat("Salsa", value);
     }
 
     private void OnEnable()
