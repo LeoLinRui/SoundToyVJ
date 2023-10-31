35738d670b4b5b043588e6386bba95043500cb17
lmhurley2003 <lmhurley@andrew.cmu.edu>
2023-10-31 01:20:29 -0400

    Misc

---
+++ Assets/PoopBehaviorScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoopBehaviorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("here");
+        if(other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("LOD") || other.gameObject.CompareTag("NPC"))  {
+            foreach(Renderer renderer in other.transform.parent.GetComponentsInChildren<Renderer>()) {
+                renderer.enabled = false;
+            }
+        } if(other.gameObject.CompareTag("Player")) {
+            Transform poopT = other.transform.parent.Find("poopChicken");
+            foreach(Renderer renderer in poopT.GetComponentsInChildren<Renderer>()) {
+                renderer.enabled = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            Transform poopT = other.transform.parent.Find("poopChicken");
+            poopT.parent = other.transform.parent.parent;
+            poopT.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
